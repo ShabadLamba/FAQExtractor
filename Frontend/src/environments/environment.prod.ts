ff9e076aab6046cfb8cd70e831e691558e63b181
ShabadLamba <shabad.l@imimobile.com>
2019-11-15 20:34:35 +0530

    Added Firebase to prod environment and ignored build folder

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA60pxujJvEyhN6SkANmkhSqUMLMaJEgDI',
+    authDomain: 'questionsandanswersdatabase.firebaseapp.com',
+    databaseURL: 'https://questionsandanswersdatabase.firebaseio.com',
+    projectId: 'questionsandanswersdatabase',
+    storageBucket: 'questionsandanswersdatabase.appspot.com',
+    messagingSenderId: '610071156368',
+    appId: '1:610071156368:web:73bb8022a2c8dedc'
+  }
 };
+
