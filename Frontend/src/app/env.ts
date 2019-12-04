37713d8ff8afc86fd68872eaf94bfc619a183647
Ubuntu <ubuntu@ip-172-16-250-234.ec2.internal>
2019-12-04 09:28:52 +0000

    Commented apiurl env.ts

--- Frontend/src/app/env.ts
+++ Frontend/src/app/env.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://dev.imibot.ai';
+export const API_URL = "http://dev.imibot.ai";
 
-// export const API_URL = 'http://127.0.0.1:5000';
-// export const API_URL = 'http://44.224.3  2.12:5000';
+// export const API_URL = "http://127.0.0.1:5000";
+// export const API_URL = 'http://44.224.32.12:5000';
