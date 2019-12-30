65ef3a5c447c2080e36c3009004665299ecca38c
ShabadLamba <shabad.l@imimobile.com>
2019-12-30 15:56:28 +0530

    Added https to env.ts

--- Frontend/src/app/env.ts
+++ Frontend/src/app/env.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://dev.imibot.ai";
+export const API_URL = "https://dev.imibot.ai";
 
 // export const API_URL = "http://0.0.0.0:5000";
 // export const API_URL = "http://127.0.0.1:5000";
